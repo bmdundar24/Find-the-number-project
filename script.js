d9e56caed6e29dd26dcdf4c11e4efd0f897769cf
Burak Mustafa Dündar <86871573+bmdundar24@users.noreply.github.com>
2021-11-03 19:20:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let feedback = document.getElementById("feedback");
 let highScore = document.getElementById("highscore");
 let myGuess = Math.floor(Math.random() * 100)+1;
 let x = 100;
+
 controlButton.addEventListener("click", () => {
   let mynumber = number.value;
   feedback.innerHTML = checkNumber(parseInt(mynumber));
